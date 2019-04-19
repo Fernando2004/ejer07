ad4a48d8df6fa90d7a14d23bddc0cff119b9022b
fernando2004 <gmfern2004@gmail.com>
2019-04-19 10:53:51 +0200

    My archivo funciona

---
+++ src/main/java/com/fernandogarcia/ejer07/Main.java
@@ -0,0 +1,12 @@
+package com.fernandogarcia.ejer07;
+
+import com.fernandogarcia.ejer07.ejer07.Ejer07;
+
+public class Main {
+
+    public static void main (String[]args){
+
+        Ejer07.principal();
+    }
+
+}
---
+++ src/main/java/com/fernandogarcia/ejer07/ejer07/Ejer07.java
@@ -0,0 +1,12 @@
+package com.fernandogarcia.ejer07.ejer07;
+
+public class Ejer07 {
+
+    public static void principal(){
+
+        System.out.println("hola mundo");
+        System.out.println("hola git");
+
+    }
+
+}
