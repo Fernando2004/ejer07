059947d687c7fda7551473e949552839f9e41c03
fernando2004 <gmfern2004@gmail.com>
2019-04-24 19:39:56 +0200

    tercer 2commit

--- src/main/java/com/fernandogarcia/ejer07/ejer07/Ejer07.java
+++ src/main/java/com/fernandogarcia/ejer07/ejer07/Ejer07.java
@@ -2,29 +2,73 @@ package com.fernandogarcia.ejer07.ejer07;
 
 import com.fernandogarcia.ejer07.utils.Lib;
 
+import java.util.InputMismatchException;
+
+import static com.fernandogarcia.ejer07.utils.Lib.lector;
+
 public class Ejer07 {
 
+    public static final int NUM_ZONAS = 24;
+
+    Zona[] arrayZonas = new Zona[NUM_ZONAS];
 
     public static void principal() {
-        //menu principal
 
-        System.out.println("Hola");
-        System.out.println(Lib.crearClave());
+        int opcion;
 
+        do {
+            opcion = menuPrincipal();
+            switch (opcion) {
+                case 1:
+                    System.out.println("--------20---------");
+                    //nuevoPaciente();
+                    break;
+                case 2:
+                    //atenderPaciente();
+                    break;
+                case 3:
+                    //consultas();
+                    break;
+                case 4:
+                    //altaMedica();
+                    break;
+                case 0:
+                    System.out.println("Hasta pronto");
+                    break;
+            }
+        } while (opcion != 0);
 
-        }
-
-    private int menuPrincipal () {
+    }
+    //--------------------Menus------------------------
+    public static int menuPrincipal() {
         int opcion = -1;
         do {
+            Lib.limpiarPantalla();
+            System.out.println("************************");
+            System.out.println("*-------- MENU --------*");
+            System.out.println("***********************");
+            System.out.println("1. ");
+            System.out.println("2. ");
+            System.out.println("3. ");
+            System.out.println("4. ");
+            System.out.println("5. ");
+            System.out.println("-----------------------");
+            System.out.println("0. Salir\n");
+            System.out.print("Elija una opción: ");
+            try {
+                opcion = Lib.leerOpcionMenu();
 
-
-        } while (opcion < 0 || opcion > 5);
+            } catch (InputMismatchException e) {
+            }
+            lector.nextLine();
+            if (opcion < 0 || opcion > 6) {
+                System.out.println("Elija una opción del menú [0-6]");
+            }
+        } while (opcion < 0 || opcion > 6);
         return opcion;
     }
 
 
-
-
-
 }
+
+
